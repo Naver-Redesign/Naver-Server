6087a4c9d76db0bb53d9069791f69e8e799bd7c2
anzun0 <zxzimin@naver.com>
2021-05-22 15:00:40 +0900

    Add: 뉴스 스키마

---
+++ src/interfaces/INews.ts
@@ -0,0 +1,7 @@
+interface INews {
+	imageURL : String,
+	title: String,
+	content: String
+}
+
+export { INews };
---
+++ src/models/news.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { INews } from "../interfaces/INews";
+
+const newsSchema = new mongoose.Schema({
+
+	imageURL: { type: String, required: true },
+	title : { type: String, required: true },
+	content : { type: String, required: true}
+});
+
+export default mongoose.model<INews & mongoose.Document>(
+  "News",
+  newsSchema
+);
